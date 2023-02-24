dae46e280d0fa3d0c23187ef9a9a435b0d32c8b7
Melissa <chopinetmelissa@live.fr>
2023-02-24 06:18:35 -0500

    Indentation du code JS

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -1,70 +1,57 @@
+/* Appel API Works*/
 
-                  /* Appel API Works*/
-
-
-          
-
-
-const worksApi = await fetch('http://localhost:5678/api/works')
+const worksApi = await fetch("http://localhost:5678/api/works");
 //console.log(worksApi);
 
- /*Mettre au format JSON */
+/*Mettre au format JSON */
 const works = await worksApi.json();
 //console.log(works);
 
-          
-  /* Creer fonction Portfolio*/
-  async function displayWorks(works) {  
-
+/* Creer fonction Portfolio*/
+async function displayWorks(works) {
   /*Choix de l'emplacement de la balise HTML*/
 
-  const divGallery = document.querySelector (".gallery");
+  const divGallery = document.querySelector(".gallery");
   //console.log(divGallery);
 
-
-    
-/*Boucle dans le tableau JSON */
-for (let i=0;i < works.length; i++) {
+  /*Boucle dans le tableau JSON */
+  for (let i = 0; i < works.length; i++) {
     //console.log(works[i].title + ', ' + works[i].imageUrl);
 
-  /*Creation des balises HTML*/
-  
-  const worksElement = document.createElement("figure");
-  worksElement.dataset.id = works[i].id;
-  worksElement.dataset.categoryId = works[i].categoryId;
-  //console.log(worksElement);
+    /*Creation des balises HTML*/
 
-  const image = document.createElement("img");
-  image.src= works[i].imageUrl;
-  image.crossOrigin = 'anonymous';
-  image.alt= works[i].title;
-  //console.log(image);
+    const worksElement = document.createElement("figure");
+    worksElement.dataset.id = works[i].id;
+    worksElement.dataset.categoryId = works[i].categoryId;
+    //console.log(worksElement);
 
-  const titleElement = document.createElement ("figcaption");
-  titleElement.innerText = works[i].title;
-  //console.log(titleElement);
+    const image = document.createElement("img");
+    image.src = works[i].imageUrl;
+    image.crossOrigin = "anonymous";
+    image.alt = works[i].title;
+    //console.log(image);
 
-  /* Lien entre les blocs enfant et parent*/
-  worksElement.appendChild(image);
-  //console.log(worksElement);
+    const titleElement = document.createElement("figcaption");
+    titleElement.innerText = works[i].title;
+    //console.log(titleElement);
 
-  worksElement.appendChild(titleElement);
+    /* Lien entre les blocs enfant et parent*/
+    worksElement.appendChild(image);
+    //console.log(worksElement);
 
-  divGallery.appendChild(worksElement);
+    worksElement.appendChild(titleElement);
+
+    divGallery.appendChild(worksElement);
     //console.log(divGallery);
-
-}
-
-
-//TENTATIVE POUR CONSTRUIRE LISTE WORKS // 
-//const categoryId = []
-
-//const result = categoryId.filter (categoryId => categoryId.length =1 );
-
-//console.log(result);
-
-
   }
+
+  //TENTATIVE POUR CONSTRUIRE LISTE WORKS //
+  //const categoryId = []
+
+  //const result = categoryId.filter (categoryId => categoryId.length =1 );
+
+  //console.log(result);
+}
 
 /*  async function filterWorks(filtreWorks){
 
@@ -106,23 +93,16 @@ await fetch('http://localhost:5678/api/works')
 
       }*/
 
-
-
-
-
-
-
-                      /* CREATION BOUTONS*/
+/* CREATION BOUTONS*/
 
 /* Creer fonction Bouton*/
-     
- const categoriesApi= await fetch(`http://localhost:5678/api/categories`)
- //console.log(categoriesApi);
 
-  /*Mettre au format JSON */
- const categories = await categoriesApi.json();
+const categoriesApi = await fetch(`http://localhost:5678/api/categories`);
+//console.log(categoriesApi);
+
+/*Mettre au format JSON */
+const categories = await categoriesApi.json();
 //console.log(categories);
-
 
 /* Creation new categorie */
 const all = {
@@ -132,109 +112,94 @@ const all = {
 
 categories.unshift(all); /*Rajouter au debut du tableau */
 
-
 /* Creer fonction Bouton*/
 
-async function displayCategories(categories) { 
+async function displayCategories(categories) {
+  /*Choix de l'emplacement de la balise HTML*/
 
+  const divCategories = document.querySelector(".filter");
+  //console.log(divCategories);
 
+  /*Boucle dans le tableau JSON */
+  for (let i = 0; i < categories.length; i++) {
+    //console.log(categories[i].name);
 
-/*Choix de l'emplacement de la balise HTML*/
+    /*Creation des balises HTML*/
 
-const divCategories = document.querySelector(".filter");
-//console.log(divCategories);
+    const buttonCategories = document.createElement("button");
+    buttonCategories.innerText = categories[i].name;
+    buttonCategories.classList.add("choice_pictures"); /*Ajout class*/
+    buttonCategories.dataset.id = categories[i].id;
 
-
-
-/*Boucle dans le tableau JSON */
-for (let i=0;i < categories.length; i++) {
-  //console.log(categories[i].name);
-
-  /*Creation des balises HTML*/
-
-  const buttonCategories = document.createElement("button");
-  buttonCategories.innerText = categories[i].name;
-  buttonCategories.classList.add("choice_pictures"); /*Ajout class*/
-  buttonCategories.dataset.id = categories[i].id;
-
-  //console.log(buttonCategories);
+    //console.log(buttonCategories);
 
     /* Lien entre les blocs enfant et parent*/
 
     divCategories.appendChild(buttonCategories);
     //console.log(divCategories);
 
-
-
-
-
-//_____________________________________________________________________________________________
-   // let bouton = document.getElementById("filter"); /*recupere bouton */
+    //_____________________________________________________________________________________________
+    // let bouton = document.getElementById("filter"); /*recupere bouton */
     //console.log(bouton);
-   // const displayCategories = (buttonCategories) 
+    // const displayCategories = (buttonCategories)
     //console.log(buttonCategories);
     //displayWorks = (works[i].categoryId)
     //console.log(works[i].categoryId)
 
-//_______________________________________________________________________________
+    //_______________________________________________________________________________
 
-// TEST DERNIERE CHANCE +++ //
+    // TEST DERNIERE CHANCE +++ //
 
-buttonCategories.addEventListener('click', () => {
-                                                 //console.log(buttonCategories)
-                                                  // const filtreButton =Array.from(filterWorks);
-                                                    //let filtreWorks= Array.from(works)
-                                                              //console.log( filtreButton);
-                                                    //filterWorks(3)
-                                              //console.log( filtreWorks);
-                                                            //filtreButton.sort (function(works){
-                                                              //return (works.id === 2 )
-                                                         //return categoryId === categories
+    buttonCategories.addEventListener("click", () => {
+      //console.log(buttonCategories)
+      // const filtreButton =Array.from(filterWorks);
+      //let filtreWorks= Array.from(works)
+      //console.log( filtreButton);
+      //filterWorks(3)
+      //console.log( filtreWorks);
+      //filtreButton.sort (function(works){
+      //return (works.id === 2 )
+      //return categoryId === categories
 
-const filteredCategories = categories.filter(category => category.id === i);
-console.log(filteredCategories);
+      const filteredCategories = categories.filter(
+        (category) => category.id === i
+      );
+      console.log(filteredCategories);
 
-const worksFiltre = fetch('http://localhost:5678/api/works')
-//console.log(worksApi);
- /*Mettre au format JSON */
+      const worksFiltre = fetch("http://localhost:5678/api/works")
+        //console.log(worksApi);
+        /*Mettre au format JSON */
+        .then((response) => response.json())
+        .then((data) => {
+          // Recuperez toute la gallery
+          data.forEach(function (works) {
+            console.log(works);
+          });
+        });
 
-
-if (filteredCategories === i ){
-
-   // Recuperez toute la gallery
- 
-
-} else if (filteredCategories === 1 ) {
-
-  // Récupérer tous les objets 
- /* let worksObjets = data.filter(function(works) {
+      if (filteredCategories === i) {
+        // Recuperez toute la gallery
+      } else if (filteredCategories === 1) {
+        // Récupérer tous les objets
+        /* let worksObjets = data.filter(function(works) {
     return works.categoryId === 1;
   });
   //console.log(worksObjets);
   document.querySelector(".gallery").innerHTML = "";
   displayWorks(worksObjets);*/
-
-} else if (filteredCategories === 2) {
-
-//Récupérer tous les Appartements
-/* let worksAppartements = data.filter(function(works) {
+      } else if (filteredCategories === 2) {
+        //Récupérer tous les Appartements
+        /* let worksAppartements = data.filter(function(works) {
   return works.categoryId === 2;
 });
 console.log(worksAppartements);
 document.querySelector(".gallery").innerHTML = "";
 displayWorks(worksAppartements);
 */
+      }
+    });
 
-}
-
-}
-
-)
-
-
-
-
-/*Penser a utiliser 
+    /*Penser a utiliser 
 if (clef === valeur){
   Afficher 
 }else if {
@@ -243,11 +208,9 @@ if (clef === valeur){
 
 }ETC... */
 
-
-
-//____________________________________________________________________________
-// DERNIER TEST 2.0 //
-/*
+    //____________________________________________________________________________
+    // DERNIER TEST 2.0 //
+    /*
   buttonCategories.addEventListener('click', () => {
     const filtreButton =Array.from(works);
     // Filtrer les résultats de l'API Fetch en utilisant la méthode filter
@@ -265,50 +228,16 @@ if (clef === valeur){
 //})
 
 /*});*/
-
-
+  }
 }
-
-}
-
-
 
 displayCategories(categories);
 //console.log(displayCategories);
 displayWorks(works);
 //console.log(displayWorks);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //____________________________________________________________________
 //ENCORE UN TEST //
-
-
 
 /*async function updateWorks(categoryId) {
 const works = await getFilteredWorks(categoryId);
@@ -366,32 +295,14 @@ for (let i = 0; i < works.length; i++) {
 //console.log(event.target.textContent);
 //}) ;
 
-
-
 //const boutonFiltre = document.querySelector(".filter");
 //boutonFiltre.addEventListener("click", function() {
 
-
 //})
-
 
 //______________________________________________________________________
 
-
-
-
-  
-  
-
-
-
-
-
-
-
-
-
-                  /*Filtre des works : Test */ 
+/*Filtre des works : Test */
 
 /*let action= document.getElementById("monBouton"); /*recupere bouton */
 //console.log(action.textContent);
@@ -399,4 +310,3 @@ for (let i = 0; i < works.length; i++) {
 /*action.addEventListener("click", (event) => {
   //console.log(event.target.textContent);
 }) /* Renvoi element Action*/
-
