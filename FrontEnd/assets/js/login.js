e519fe5d688dae9d09f92b46e5b5595452053b32
Melissa <chopinetmelissa@live.fr>
2023-02-26 10:59:31 -0500

    Mise en place des differents lien login et index

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -8,7 +8,8 @@ let login = await fetch("http://localhost:5678/api/users/login", {
   //console.log(login);
   method: "POST",
   headers: {
-    "Content-Type": "application/json;charset=utf-8",
+    "Content-Type": "application/json",
+                      "charset=utf-8",
   },
   body: JSON.stringify(user),
 });
