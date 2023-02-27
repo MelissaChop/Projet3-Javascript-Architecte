1d6d6a1f0f317f1b73a68559adaab05d43102637
Melissa <chopinetmelissa@live.fr>
2023-02-27 07:53:09 -0500

    Recuperation du Token

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,46 +1,44 @@
 //Recuperation des elements du formulaire//
-const loginForm = document.querySelector(".loginForm");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-//console.log(loginForm);
-//console.log(email);
-//console.log(password);
 
-let user = {
-  email: "loginForm.email.value",
-  password: "loginForm.password.value",
+//Sélectionner le texte saisi dans le contrôle.//
+
+document.getElementById("selectAll").onclick = function (event) {
+  event.preventDefault(); // Permet de ne pas rediriger//
+
+  // Recuperation API Login //
+  let loginIn = fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    Authorization: `Bearer ${password}`,
+    body: JSON.stringify({
+      email: `${email.value}`,
+      password: `${password.value}`,
+    }),
+  })
+    .then((reponse) => reponse.json())
+    .then((data) => console.log(data));
+
+  //window.location.href = "./index.html";
+
+  window.localStorage.setItem(
+    "token",
+    `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4`
+  );
 };
-//console.log(user);
 
-// Recuperation API Login //
-
-let loginIn = await fetch("http://localhost:5678/api/users/login", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json;charset=utf-8",
-  },
-  Authorization: `Bearer ${password}`,
-  body: JSON.stringify(user),
-});
-
-//console.log(loginIn);
-
-const enterLogin = await loginIn.json();
+//const enterLogin = await loginIn.json();
 //alert(enterLogin.message);
 
-//console.log(enterLogin);
-async function userLog(enterLogin) {
+/*async function userLog(enterLogin) {
   // Recuperation ID //
   const userId = window.sessionStorage.getItem(userId);
   // Test recuperation TOken
   const token =
     window.sessionStorage.getItem(token); /*SessionStorage : reste actif le 
 temsps que la page est lance, incluant les rechargement et restauration de page*/
-}
+/*}*/
 //console.log(userLog);
-
-//Sélectionner le texte saisi dans le contrôle.//
-
-document.getElementById("selectAll").onclick = function (event) {
-  document.getElementById("password").select();
-};
