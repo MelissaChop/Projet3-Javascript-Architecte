9b0487170385949258b4a654f16a272747be106d
Melissa <chopinetmelissa@live.fr>
2023-02-28 22:43:03 -0500

    Stokage du Token

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -5,7 +5,7 @@ const password = document.getElementById("password");
 //Sélectionner le texte saisi dans le contrôle.//
 
 document.querySelector(".loginForm").onsubmit = function (event) {
-  // event.preventDefault(); // Permet de ne pas rediriger//
+  event.preventDefault(); // Permet de ne pas rediriger//
 
   // Recuperation API Login //
   fetch("http://localhost:5678/api/users/login", {
@@ -21,24 +21,25 @@ document.querySelector(".loginForm").onsubmit = function (event) {
     .then((reponse) => {
       if (!reponse.ok) {
         if (reponse.status === 404) {
-          alert((reponse.message = "Non trouvé"));
-          console.log("test" + reponse.status);
+          alert("Utilisateur non trouvé");
+          //console.log("test" + reponse.status);
         } else if (reponse.status === 401) {
-          alert((reponse.message = "Utilisateur non autorisé"));
-        } else {
-          throw new error("Erreur détectée!");
+          alert("Utilisateur non autorisé");
         }
-      } else {
-        return reponse.json();
+        throw new Error("Erreur détectée!");
       }
-    })
-    .then((data) => {
-      console.log(data);
-      window.sessionStorage.setItem("user", JSON.stringify(data));
-      if ((data.userId = 1)) {
-        window.location.href = "./index.html";
-      }
+      return reponse.json();
     })
 
-    .catch((error) => console.log(error));
+    .then((data) => {
+      console.log(data);
+      window.sessionStorage.setItem("Token", JSON.stringify(data.token));
+      // window.sessionStorage.setItem("User", data.userId);
+      console.log(window.sessionStorage);
+
+      let token = window.sessionStorage.getItem("Token");
+      console.log(token);
+    })
+
+    .catch((error) => console.error(error));
 };
