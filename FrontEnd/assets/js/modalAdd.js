3e4852ac2c40afc0c4dd3a3b67a284e9e7399015
Melissa <chopinetmelissa@live.fr>
2023-03-11 13:40:52 -0500

    Add photo first

--- FrontEnd/assets/js/modalAdd.js
+++ FrontEnd/assets/js/modalAdd.js
@@ -1,13 +1,43 @@
 // Ajout Pictures
-
-const addPicturesForm = document.querySelector(".modal-content2");
+/*let addPicturesForm = document.querySelector(".modal-content2");
 addPicturesForm.addEventListener("submit", addPicture);
 
 function addPicture(e) {
   e.preventDefault();
   //console.log("form submit"); ( NE FONCITONNE PAS)
 
-  const form = new FormData(addPicturesForm);
-  const titre = formData.get("#title");
-  const categ = formData.get("#selectionCategorie2");
-}
+  let formData = new FormData(addPicturesForm);
+  let titre = formData.get("#title");
+  let categ = formData.get("#selectionCategorie2");
+
+  console.log("Pictures", { titre, categ });
+}*/
+
+const token = window.sessionStorage.getItem("User");
+const tokenObj = JSON.parse(token);
+//console.log(tokenObj);
+
+let addPicturesForm = document.querySelector(".modal-content2");
+addPicturesForm.onsubmit = async (e) => {
+  e.preventDefault();
+  // console.log("form submit");
+
+  let response = await fetch(`http://localhost:5678/api/works/${id}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Accept: "application/json",
+      Authorization: `Bearer ${token.token}`,
+    },
+    body: new FormData(addPicturesForm),
+  });
+  let result = await response.json();
+
+  alert(result.message);
+};
+
+/*let formData = new FormData([form]);
+let titre = formData.get("#title");
+let categ = formData.get("#selectionCategorie2");
+
+console.log("Pictures", { titre, categ });*/
