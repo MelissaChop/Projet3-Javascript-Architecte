e16dd5e19bbc5b3648d66193c6bd1a0e7137d7af
Melissa <chopinetmelissa@live.fr>
2023-03-19 19:47:21 -0400

    Ajout des categories de la ModalAdd en JS en non HTML

--- FrontEnd/assets/js/modalAdd.js
+++ FrontEnd/assets/js/modalAdd.js
@@ -1,6 +1,29 @@
 import { displayWorksD } from "./modalMEP.js";
 import { displayWorks } from "./index.js";
 
+//FILTRE EN DYNAMIQUE
+const categorieModal = document.querySelector("#selectionCategorie2");
+
+const filtreModal = document.createElement("option");
+filtreModal.value = "0";
+const filtreModal1 = document.createElement("option");
+filtreModal1.value = "1";
+filtreModal1.innerText = "Objets";
+const filtreModal2 = document.createElement("option");
+filtreModal2.value = "2";
+filtreModal2.innerText = "Appartements";
+const filtreModal3 = document.createElement("option");
+filtreModal3.value = "3";
+filtreModal3.innerText = "Hôtels & restaurants";
+
+categorieModal.appendChild(filtreModal);
+categorieModal.appendChild(filtreModal1);
+categorieModal.appendChild(filtreModal2);
+categorieModal.appendChild(filtreModal3);
+
+console.log(categorieModal);
+
+//---------------------------------------------------------
 let bouttonValid = document.querySelector("#addPictures");
 let inputImage = document.querySelector("#image");
 let inputTitle = document.querySelector("#title");
